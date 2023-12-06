c111a18fd7a8d3db202580fa292af15bb7f4c38d
SooHwanC <frcp9408@gmail.com>
2023-12-07 00:56:42 +0900

    1207_새벽

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -62,10 +62,14 @@ function Main() {
                 </div>
                 <ul className="nav_box_right">
                     <li>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" class="svg-inline--fa fa-github " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
+                        <a href='https://github.com/SooHwanC' target="_blank" rel="noopener noreferrer">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" class="svg-inline--fa fa-github " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
+                        </a>
                     </li>
                     <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 459 459"><title>티스토리 로고</title><g><path d="M229.5,0C102.75,0,0,102.75,0,229.5S102.75,459,229.5,459,459,356.25,459,229.5,356.25,0,229.5,0ZM130.21,191.45a39.57,39.57,0,1,1,39.56-39.57A39.58,39.58,0,0,1,130.21,191.45ZM229.5,390a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,390Zm0-99.29a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,290.74Zm0-99.29a39.57,39.57,0,1,1,39.56-39.57A39.57,39.57,0,0,1,229.5,191.45Zm99.29,0a39.57,39.57,0,1,1,39.57-39.57A39.57,39.57,0,0,1,328.79,191.45Z" /></g></svg>
+                        <a href='https://frcp9408.tistory.com/' target="_blank" rel="noopener noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 459 459"><title>티스토리 로고</title><g><path d="M229.5,0C102.75,0,0,102.75,0,229.5S102.75,459,229.5,459,459,356.25,459,229.5,356.25,0,229.5,0ZM130.21,191.45a39.57,39.57,0,1,1,39.56-39.57A39.58,39.58,0,0,1,130.21,191.45ZM229.5,390a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,390Zm0-99.29a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,290.74Zm0-99.29a39.57,39.57,0,1,1,39.56-39.57A39.57,39.57,0,0,1,229.5,191.45Zm99.29,0a39.57,39.57,0,1,1,39.57-39.57A39.57,39.57,0,0,1,328.79,191.45Z" /></g></svg>
+                        </a>
                     </li>
                     <li>
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
@@ -105,19 +109,22 @@ function Main() {
                             <img src="/profile_pic.jpg" />
                         </div>
                         <div className="second_block_intro">
-                            <div>
-                                Full Stack Developer
-                            </div>
-                            <div>
-                                name : 최수환
-                            </div>
-                            <div>
-                                Date of Birth : 08/22/1994
-                            </div>
-                            <div>
-                                Keyworlds : 차분함 꼼꼼함 완벽함
-                            </div>
-
+                            <h1>
+                                To Be <br />Full Stack Developer
+                            </h1>
+                            <p>
+                                안녕하세요 풀스택 개발자를 지향하는 최수환입니다. <br />
+                                새로운 지식과 기술을 배우는걸 좋아합니다. <br />
+                                완벽을 추구하는 성향 덕분에 맡은 일을 끝까지 해냅니다. <br />
+                            </p>
+                            <ul>
+                                <li>
+                                    #꼼꼼함
+                                </li>
+                                <li>
+                                    #완벽주의
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </SwiperSlide >
@@ -126,18 +133,27 @@ function Main() {
                         <div className='left_box'>
                         </div>
                         <div className='right_box'>
-                            <div className='right_box_front'>
+                            <h1>
                                 Front
-                            </div>
+                            </h1>
                             <div className='logo_wrapper'>
+                                <h2>
+                                    Language
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/html.svg' alt='logo_html' class='tilt-animation' />
                                     <img src='/css.svg' alt='logo_css' class='tilt-animation' />
                                     <img src='/javascript.svg' alt='logo_js' class='tilt-animation' />
                                 </div>
+                                <h2>
+                                    FrameWork
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/react.svg' alt='logo_react' class='tilt-animation' />
                                 </div>
+                                <h2>
+                                    Library
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/redux.svg' alt='logo_redux' class='tilt-animation' />
                                     <img src='/sass.svg' alt='logo_sass' class='tilt-animation' />
@@ -152,15 +168,21 @@ function Main() {
                         <div className='left_box'>
                         </div>
                         <div className='right_box'>
-                            <div className='right_box_front'>
+                            <h1>
                                 Back
-                            </div>
+                            </h1>
                             <div className='logo_wrapper'>
+                                <h2>
+                                    Language
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/javascript.svg' alt='logo_js' class='tilt-animation' />
                                     <img src='/java.svg' alt='logo_java' class='tilt-animation' />
                                     <img src='/python.svg' alt='logo_python' class='tilt-animation' />
                                 </div>
+                                <h2>
+                                    FrameWork
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/springboot.svg' className='large_svg' alt='logo_spb' />
                                     <img src='/node1.svg' className='large_svg' alt='logo_node' />
@@ -175,15 +197,21 @@ function Main() {
                         <div className='left_box'>
                         </div>
                         <div className='right_box'>
-                            <div className='right_box_front'>
+                            <h1>
                                 Etc
-                            </div>
+                            </h1>
                             <div className='logo_wrapper'>
+                                <h2>
+                                    DataBase
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/oracle.svg' alt='logo_oracle' class='tilt-animation' />
                                     <img src='/mysql.svg' alt='logo_mysql' class='tilt-animation' />
                                     <img src='/mongodb.svg' alt='logo_mongodb' class='tilt-animation' />
                                 </div>
+                                <h2>
+                                    Etc
+                                </h2>
                                 <div className='right_box_logo'>
                                     <img src='/docker.svg' alt='logo_docker' class='tilt-animation' />
                                     <img src='/firebase.svg' alt='logo_firebase' class='tilt-animation' />
@@ -208,13 +236,49 @@ function Main() {
                             <div className='port_box'>
                                 <div className='port_box_inner'>
                                     <div className='port_box_img'>
-                                        <img src='/javaburger.png' alt='img_javaburger' />
+                                        <img src='/itty.png' alt='img_itty' />
                                     </div>
                                     <div className='port_box_description'>
                                         <div>
-                                            <h2>Java Burger</h2>
+                                            <h2>ITTY</h2>
                                             <span>
-                                                햄버거를 만들어 사람들에게 행복을 전달하는 본격! 햄버거 타이쿤
+                                                개발자를 위한 IT커뮤니티 ITTY
+                                            </span>
+                                            <ul>
+                                                <li>
+                                                    React
+                                                </li>
+                                                <li>
+                                                    Node.js
+                                                </li>
+                                                <li>
+                                                    MongoDB
+                                                </li>
+                                                <li>
+                                                    Node.js
+                                                </li>
+                                            </ul>
+                                            <a href='https://www.notion.so/ITTY-f5805f363bde4b32ab54562aa3e8c14f?pvs=4' target="_blank" rel="noopener noreferrer">
+                                                <div className='detail_btn'>
+                                                    Detail
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='port_box'>
+                                <div className='port_box_inner'>
+                                    <div className='port_box_img'>
+                                        <img src='/codebridge2.jpg' alt='img_codebridge' />
+                                    </div>
+                                    <div className='port_box_description'>
+                                        <div>
+                                            <h2>Code Bridge</h2>
+                                            <span>
+                                                웹 개발환경과 AI가 포함된 코딩 AI LMS
                                             </span>
                                             <ul>
                                                 <li>
@@ -224,10 +288,10 @@ function Main() {
                                                     Java
                                                 </li>
                                             </ul>
-                                        </div>
-                                        <div className='detail_btn'>
-                                            <a href='https://bevel-amusement-3e9.notion.site/JAVA-BURGER-4fb6023011fe40d29dd356bd2afe73b4?pvs=4' target="_blank" rel="noopener noreferrer">
-                                                Detail
+                                            <a href='https://www.notion.so/CodeBridge-365c3985ce13457abfda09b8a96a5406?pvs=4' target="_blank" rel="noopener noreferrer">
+                                                <div className='detail_btn'>
+                                                    Detail
+                                                </div>
                                             </a>
                                         </div>
                                     </div>
@@ -254,9 +318,11 @@ function Main() {
                                                     Java
                                                 </li>
                                             </ul>
-                                            <div className='detail_btn'>
-                                                Detail
-                                            </div>
+                                            <a href='https://www.notion.so/Travel-Maker-53ef2bbc38e14762b2d8be9c94a52690?pvs=4' target="_blank" rel="noopener noreferrer">
+                                                <div className='detail_btn'>
+                                                    Detail
+                                                </div>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -266,13 +332,13 @@ function Main() {
                             <div className='port_box'>
                                 <div className='port_box_inner'>
                                     <div className='port_box_img'>
-                                        <img src='/itty.png' alt='img_itty' />
+                                        <img src='/javaburger.png' alt='img_javaburger' />
                                     </div>
                                     <div className='port_box_description'>
                                         <div>
-                                            <h2>ITTY</h2>
+                                            <h2>Java Burger</h2>
                                             <span>
-                                                개발자를 위한 IT커뮤니티 ITTY
+                                                햄버거를 만들어 사람들에게 행복을 전달하는 본격! 햄버거 타이쿤
                                             </span>
                                             <ul>
                                                 <li>
@@ -282,38 +348,12 @@ function Main() {
                                                     Java
                                                 </li>
                                             </ul>
+                                        </div>
+                                        <a href='https://bevel-amusement-3e9.notion.site/JAVA-BURGER-4fb6023011fe40d29dd356bd2afe73b4?pvs=4' target="_blank" rel="noopener noreferrer">
                                             <div className='detail_btn'>
                                                 Detail
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className='port_box'>
-                                <div className='port_box_inner'>
-                                    <div className='port_box_img'>
-                                        <img src='/codebridge2.jpg' alt='img_codebridge' />
-                                    </div>
-                                    <div className='port_box_description'>
-                                        <div>
-                                            <h2>Code Bridge</h2>
-                                            <span>
-                                                웹 개발환경과 AI가 포함된 코딩 AI LMS
-                                            </span>
-                                            <ul>
-                                                <li>
-                                                    Java
-                                                </li>
-                                                <li>
-                                                    Java
-                                                </li>
-                                            </ul>
-                                            <div className='detail_btn'>
-                                                Detail
-                                            </div>
-                                        </div>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
