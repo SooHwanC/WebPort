fc14e7abb46201cccfb11c8df0b23d7d8cb154af
SooHwanC <frcp9408@gmail.com>
2024-07-26 07:27:39 +0900

    cm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import './reset.css';
+// import './reset.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import JavaBurger from './pages/JavaBurger';
+import CircleSlider from './pages/CircleSlider';
 
 
 function App() {
 
   return (
-      <>
+    <>
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/detail/1' element={<JavaBurger />} />
-
+        <Route path='/CircleSlider' element={<CircleSlider />} />
       </Routes>
-      </>
+    </>
   );
 }
 
