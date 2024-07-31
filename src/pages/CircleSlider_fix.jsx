ce7680257ad8a6d4ff201d117a2d6acfaac1199a
SooHwanC <frcp9408@gmail.com>
2024-07-31 15:07:47 +0900

    css개선

--- src/pages/CircleSlider_fix.jsx
+++ src/pages/CircleSlider_fix.jsx
@@ -6,29 +6,55 @@ import '../css/CircleSlider.scss';
 
 const CircularSlider = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [sliderSize, setSliderSize] = useState(800);
+    const slideSize = 200;
 
     const slides = [
-        { image: '/slider/js.svg', title: 'Javascript' },
-        { image: '/slider/css.png', title: 'Css3' },
-        { image: '/slider/html.png', title: 'Html5' },
-        { image: '/slider/sass.png', title: 'Sass' },
-        { image: '/slider/vstudio.png', title: 'MVS Code' },
-        { image: '/slider/chrome.png', title: 'Chrome' },
+        { image: '/slider/slide_jobtalks.svg', title: 'JOB TALKS' },
+        { image: '/slider/slide_itty.svg', title: 'ITTY' },
+        { image: '/slider/slide_codebridge.svg', title: 'CODE BRIDGE' },
+        { image: '/slider/slide_travlemaker.svg', title: 'TRAVEL MAKER' },
+        { image: '/slider/slide_javaburger.svg', title: 'JAVA BURGER' },
     ];
 
     const stepAngle = (2 * Math.PI) / slides.length;
-    const sliderSize = 400; // 픽셀 단위의 고정 크기
-    const slideSize = 60;  // 픽셀 단위의 고정 크기
 
     const [{ rotate }, api] = useSpring(() => ({
-        rotate: 0,
-        config: { mass: 1, tension: 120, friction: 14 },
+        rotate: -45,
+        config: { mass: 2, tension: 100, friction: 14 },
     }));
 
+    const calculateSliderSize = () => {
+        const viewportWidth = window.innerWidth;
+        console.log('viewportWidth:', viewportWidth);
+        const viewportHeight = window.innerHeight;
+        const minDimension = Math.min(viewportWidth, viewportHeight);
+        return Math.min(minDimension * 0.8, 800);
+    };
+
     useEffect(() => {
-        const newRotate = -currentSlide * (stepAngle * 180 / Math.PI);
-        api.start({ rotate: newRotate, immediate: false });
-    }, [currentSlide, api, stepAngle]);
+        const handleResize = () => {
+            setSliderSize(calculateSliderSize());
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    useEffect(() => {
+        let newRotate = -currentSlide * (stepAngle * 180 / Math.PI);
+        let currentRotate = rotate.get();
+        let diff = newRotate - currentRotate;
+        
+        // 가장 짧은 회전 경로 선택
+        if (Math.abs(diff) > 180) {
+          diff -= Math.sign(diff) * 360;
+        }
+        
+        api.start({ rotate: currentRotate + diff, immediate: false });
+      }, [currentSlide, api, stepAngle, rotate]);
 
     const rotateSlider = (direction) => {
         const newSlide = (currentSlide - direction + slides.length) % slides.length;
@@ -37,7 +63,7 @@ const CircularSlider = () => {
 
     const bind = useDrag(({ movement: [mx], velocity: [vx], active, last }) => {
         if (active) {
-            const rotation = -currentSlide * (stepAngle * 180 / Math.PI) + mx * 2;
+            const rotation = -currentSlide * (stepAngle * 180 / Math.PI) + mx / 4;
             api.start({ rotate: rotation, immediate: true });
         } else if (last) {
             const moveThreshold = 1;
@@ -53,19 +79,13 @@ const CircularSlider = () => {
         }
     }, { axis: 'x', preventScroll: true });
 
+    const initialRotation = -45 * (Math.PI / 180);
+    
+
     return (
-        <div className="slider" >
+        <div className="slider">
             <div className="circular-slider circular-slider-1">
                 <div className="wrapper">
-                    {/* 수동조작 */}
-                    {/* <div className="controls">
-                        <div className="controls__left" onClick={() => rotateSlider(1)}>
-                            <div className="icon-wrapper"><i className="far fa-arrow-alt-circle-left"></i></div>
-                        </div>
-                        <div className="controls__right" onClick={() => rotateSlider(-1)}>
-                            <div className="icon-wrapper"><i className="far fa-arrow-alt-circle-right"></i></div>
-                        </div>
-                    </div> */}
                     <div className="descriptions">
                         {slides.map((slide, index) => (
                             <div key={index} className={`descriptions__item ${index === currentSlide ? 'descriptions__item_visible' : ''}`} style={{ display: index === currentSlide ? 'block' : 'none' }}>
@@ -73,52 +93,49 @@ const CircularSlider = () => {
                             </div>
                         ))}
                     </div>
-                    <animated.div
-                        className="slides-holder"
-                        style={{
-                            width: `${sliderSize}px`,
-                            height: `${sliderSize}px`,
-                            position: 'absolute',
-                            top: '50%',
-                            left: '50%',
-                            transform: rotate.to(r => `translate(0%, 0%) rotate(${r}deg)`),
-                            touchAction: 'none',
-                        }}
-                    >
-                        {slides.map((slide, index) => {
-                            const angle = stepAngle * index;
-                            const radius = (sliderSize / 2 - slideSize / 2) * 0.8; // 0.8을 조정하여 반지름 변경
-                            const x = radius * Math.cos(angle - Math.PI / 2);
-                            const y = radius * Math.sin(angle - Math.PI / 2);
-                            return (
-                                <div
-                                    key={index}
-                                    className={`slides-holder__item ${index === currentSlide ? 'slides-holder__item_active' : ''}`}
-                                    style={{
-                                        width: `${slideSize}px`,
-                                        height: `${slideSize}px`,
-                                        position: 'absolute',
-                                        top: '50%',
-                                        left: '50%',
-                                        transform: `translate(calc(-50% + ${x}px), calc(-50% + ${y}px)) rotate(${angle * 180 / Math.PI}deg)`,
-                                    }}
-                                >
-                                    <img
-                                        src={slide.image}
-                                        alt={slide.title}
+                    <div className="slides_wrapper">
+                        <animated.div
+                            className="slides-holder"
+                            style={{
+                                width: `${sliderSize}px`,
+                                height: `${sliderSize}px`,
+                                transform: rotate.to(r => `translate(0%, 0%) rotate(${r}deg)`),
+                                touchAction: 'none',
+                            }}
+                        >
+                            {slides.map((slide, index) => {
+                                const angle = stepAngle * index + initialRotation;
+                                const slideSize = sliderSize * 0.15; // 슬라이더 크기의 15%로 설정
+                                const radius = (sliderSize / 2 - slideSize / 2) * 1.2; // 0.8을 조정하여 반지름 변경
+                                const x = radius * Math.cos(angle - Math.PI / 2);
+                                const y = radius * Math.sin(angle - Math.PI / 2);
+                                return (
+                                    <div
+                                        key={index}
+                                        className={`slides-holder__item ${index === currentSlide ? 'slides-holder__item_active' : ''}`}
                                         style={{
-                                            width: '100%',
-                                            height: '100%',
-                                            cursor: 'grab',
-                                            touchAction: 'none'
+                                            // width: `${slideSize}px`,
+                                            // height: `${slideSize}px`,
+
+                                            transform: `translate(calc(-50% + ${x}px), calc(-50% + ${y}px)) rotate(${angle * 180 / Math.PI}deg)`,
                                         }}
-                                        {...(index === currentSlide ? bind() : {})}
-                                    />
-                                </div>
-                            );
-                        })}
-                    </animated.div>
-                  
+                                    >
+                                        <img
+                                            src={slide.image}
+                                            alt={slide.title}
+                                            style={{
+                                                width: '100%',
+                                                height: '100%',
+                                                cursor: 'grab',
+                                                touchAction: 'none'
+                                            }}
+                                            {...(index === currentSlide ? bind() : {})}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </animated.div>
+                    </div>
                 </div>
             </div>
         </div>
